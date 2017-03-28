752e49ef5b92162de7f8036769aae1a2ee4355c2
zhouxin <znz.zhouxin@gmail.com>
2017-03-28 09:41:41 +0800

    add user - create/show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,9 @@
 class UsersController < ApplicationController
+  def create
+    @user = User.create_user_by_params(params)
+  end
+
+  def show
+    @user = User.query_user_by_params(params)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,23 @@
 class User < ApplicationRecord
+  def self.create_user_by_params(params)
+    user = nil
+    username, password, realname, phone, email = params.values_at(:username, :password, :realname, :phone, :email)
+    if (username && password && realname).present?
+      user = User.create
+      user.username = username
+      user.password = password
+      user.realname = realname
+      user.phone = phone
+      user.email = email
+      user.save!
+    end
+    user
+  end
+
+  def self.query_user_by_params(params)
+    user = nil
+    id = params[:id]
+    user = User.find_by_id(id)
+    user
+  end
 end
